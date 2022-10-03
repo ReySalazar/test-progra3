58e9be0d2da6ae1e57b48a405200ba0dcd38eba0
rsalazar1409 <rsalazar@navent.com>
2022-10-03 20:24:47 -0300

    solucion package json

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-//console.log(process.argv)
-//console.log(`Hola clase de ${process.argv[2]}`);
-/*const fechaProgramador = new Date(2022, 08, 13);
-const fechaActual = new Date();
-
-if (fechaActual > fechaProgramador) {
-    console.log("El día del programador ya fué");
-} else if (fechaActual < fechaProgramador) {
-    console.log("El día del programador todavía no llegó");
-} else {
-    console.log("Es hoy, es hoy, es hoy");
-}*/
+const date_fns = require('date-fns');
+//const {isAfter} = require('date-fns');  // otra forma de importar la libreria
 
 const [dia, mes, anio] = [process.argv[2], process.argv[3], process.argv[4]]; 
 const fechaIngresada = new Date(anio, mes - 1, dia);
 const fechaActual = new Date();
 
 console.log(
-    fechaIngresada > fechaActual ? "la fecha aun no paso" : "la fecha ya paso"
+    //fechaIngresada > fechaActual ? "la fecha aun no paso" : "la fecha ya paso"
+    date_fns.isAfter(fechaIngresada, fechaActual) ? "la fecha aun no paso" : "la fecha ya paso"
 )
 // npm i date-fns-tz@1.1.1  ^ Actualizable, [sin nada] no actulaiza, ~ actualiza el minor
 // git stash save -m "mensaje" ##  git stash pop - git stash apply  
